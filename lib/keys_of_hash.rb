239722362ae6ca4ba242cc36504d64bb450e263d
Nathan Bell <nathanbell09@gmail.com>
2018-11-10 20:06:42 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,9 +1,6 @@
-require "pry"
-
 class Hash
   def keys_of(*arguments)
     # code goes here
-    return arguments.class
-    collect {|k, v| arguments == v ? k : nil}.compact
+    collect {|k, v| arguments.include?(v) ? k : nil}.compact
   end
 end
